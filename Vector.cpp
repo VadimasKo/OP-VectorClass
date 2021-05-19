a0925c0f39f46cdccebf4f8b73534480305a898e
VadimasKo <kozemiakinas123@gmail.com>
2021-05-19 19:42:40 +0300

    add insert()?

--- Vector.cpp
+++ Vector.cpp
@@ -1,78 +1,150 @@
-
 #include <iostream>
+#include <vector>
 
 
 template<typename T>
-class Vector{
-    public: //constructors, destructors, assing operators
-        Vector(){
-            allocate(2);
-            std::cout<<"Construct \n";
-        }
+class vector{
+    public:
+    //type definitions
+        typedef T* iterator;
+        typedef const T* const_iterator;
+        typedef T value_type;
 
-        ~Vector(){
+
+    // Constructors, destructors, assignement operators
+        vector(){
+            allocate(2);
+        }
+        
+        ~vector(){
             delete[] data;
         }
 
+        vector& operator= (const vector& x){
+            //copy parameters of x vector
+            this->m_capacity = x.capacity();
+            this->m_size = x.size();
+            
+            //creating vector with the same parameters
+            T* newBlock = new T[this->m_capacity];
 
-    public: //capacity
-        size_t size() const{
-            return size;
-        }
-
-
-    
-    public: //modifiers
-        void push_back(const T& value){
-            std::cout<<"pushback \n";
-
-            // checking for free capacity, if not expanding array
-            if(container_size >= capacity){
-                allocate(capacity+capacity/2);
+            //Copy values into new vector
+            for(size_t i = 0; i < this->m_size; i++){
+                newBlock[i] = x[i];
             }
 
-            data[container_size] = value;
-            container_size++;
+            //delete old vector and assign new one
+            delete[] this->data;
+            this->data = newBlock;
+
+            return *this;
         }
-    
-    public: //element acess
+
+
+    // Iterators
+
+    // Capacity
+        size_t capacity() const{
+            return m_capacity;
+        }
+
+        size_t size() const{
+            return m_size;
+        }
+
+    // Element acess
         T& operator[] (size_t index) const{
             return data[index];
         }
 
 
-    private: 
-        void allocate(size_t newCapacity){
-            std::cout<<"Allocate \n";
-
-            //creating new block
-            T* newBlock = new T[newCapacity]; 
-            // copying previuos elements into new array
-            if(newCapacity < container_size) container_size = newCapacity;
-            for(size_t i=0; i < container_size; i++){
-                newBlock[i] = data[i];
+    // Modifiers 
+        void push_back(const T& value){
+            // checking for free m_capacity, if not expanding array
+            if(m_size >= m_capacity){
+                allocate(m_capacity+m_capacity/2);
             }
-            //deleting old array and assinging new one
-            delete[] data;
-            data = newBlock;
-            capacity = newCapacity;
+
+            data[m_size] = value;
+            m_size++;
         }
 
+        void  pop_back(){
+            //  remove last element of array
+            //  after testing, std::vector seems to just decrease array size
+            m_size -=1;
+            
+        }
+
+        iterator insert(const_iterator position, const value_type& val){
+            //insert one element into array
+
+            //creating new array
+            T* newBlock = new T[size+1];
+            // geting index value of position
+            int index = std::distance(data,position);
+            for(size_t i = 0; i<size; i++){
+                if(i > index) newBlock[i+1] = data[i];
+                else if(i == index) newBlock[i] = val;
+                else newBlock[i] = data[i];
+            }
+            
+            //deleting old array
+            delete[] data;
+            //assing new array
+            data = newBlock;
+            //update size;
+            size +=1; 
+            
+            return data[index];
+            
+        }
+
+    // Allocator
+
+    
+
+
     private:
+    //members
         T* data = nullptr;
-        size_t container_size = 0;
-        size_t capacity = 0;
+        size_t m_size = 0;
+        size_t m_capacity = 0;
+    // private methods
+        void allocate(size_t newCapacity){
+                //creating new block
+                T* newBlock = new T[newCapacity]; 
+                // copying previuos elements into new array
+                if(newCapacity < m_size) m_size = newCapacity;
+                for(size_t i=0; i < m_size; i++){
+                    newBlock[i] = data[i];
+                }
+                //deleting old array and assinging new one
+                delete[] data;
+                data = newBlock;
+                m_capacity = newCapacity;
+        }
 };
 
-
-
-
 int main(){ 
-    Vector<int> container;
+    vector<int> container;
+    vector<int> container2;
     int a = 1;
-    container.push_back(1);
-    container.push_back(2);
-    container.push_back(3);
-    std::cout<<container[0]<<container[1]<<container[2];
+    container2.push_back(1);
+    container2.push_back(2);
+    container2.push_back(3);
+
+    container = container2;
+    // container2[1] = 4;
+
+    container.insert(*container[1]; 9);
+
+    // vector<std::string> stringContainer;
+    // stringContainer.push_back("Abc");
+    // stringContainer.push_back("EBC");
+
+    // std::cout<<stringContainer[0]<<stringContainer[1]<<"\n";
+
+    std::cout<<container[0]<<container[1]<<container[2]<<container[3]<<"\n";
 
 }
